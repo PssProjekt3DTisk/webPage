ea0caa74ea4cdbf7965ed9c38c2da95e7b924c18
PssProjekt3DTisk <125122584+PssProjekt3DTisk@users.noreply.github.com>
2023-04-10 17:50:38 +0100

    more

--- main.js
+++ main.js
@@ -4,55 +4,55 @@ let file;
 let describtion;
 
 document.querySelector("form").addEventListener("submit", function (e) {
-  e.preventDefault();
+    e.preventDefault();
 });
 
 function checkInputs(form) {
-  name = form.name.value;
-  email = form.email.value;
-  file = form.file.files[0];
-  description = form.description.value;
+    name = form.name.value;
+    email = form.email.value;
+    file = form.file.files[0];
+    description = form.description.value;
 
-  if (file.size < 70 * 1024) {
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function () {
-      var obj = new XMLHttpRequest();
-    
-      if (name !== "" && email !== "" && file.name !== "") {
-        if (
-          name.length > 0 &&
-          email.length > 0 &&
-          email.includes("@") &&
-          String(file.name).includes(".stl")
-        ) {
-          obj.open("post", form.action, true);
-          obj.setRequestHeader("Content-Type", "application/json");
+    if (file.size < 70 * 1024) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            var obj = new XMLHttpRequest();
 
-          obj.send(
-            JSON.stringify({
-              name: name,
-              filename: file.name,
-              file: reader.result,
-              email: email,
-              description: description,
-            })
-          );
+            if (name !== "" && email !== "" && file.name !== "") {
+                if (
+                    name.length > 0 &&
+                    email.length > 0 &&
+                    email.includes("@") &&
+                    String(file.name).includes(".stl")
+                ) {
+                    obj.open("post", form.action, true);
+                    obj.setRequestHeader("Content-Type", "application/json");
 
-          form.name.value = "";
-          form.email.value = "";
-          form.file.value = "";
-          form.description.value = "";
+                    obj.send(
+                        JSON.stringify({
+                            name: name,
+                            filename: file.name,
+                            file: reader.result,
+                            email: email,
+                            description: description,
+                        })
+                    );
 
-          return false;
-        } else {
-          alert("NÄ›kterÃ© z polÃ­ je Å¡patnÄ› zadÃ¡no! Zkuste to prosÃ­m znovu.");
-        }
-      } else {
-        alert("NÄ›kterÃ© z polÃ­ je Å¡patnÄ› zadÃ¡no! Zkuste to prosÃ­m znovu.");
-      }
-    };
-  } else {
-    alert("Soubor je moc velkÃ½! Vyberte prosÃ­m menÅ¡Ã­.");
-  }
+                    form.name.value = "";
+                    form.email.value = "";
+                    form.file.value = "";
+                    form.description.value = "";
+
+                    return false;
+                } else {
+                    alert("Nìkteré z polí je špatnì zadáno! Zkuste to prosím znovu.");
+                }
+            } else {
+                alert("Nìkteré z polí je špatnì zadáno! Zkuste to prosím znovu.");
+            }
+        };
+    } else {
+        alert("Soubor je moc velký! Vyberte prosím menší.");
+    }
 }
